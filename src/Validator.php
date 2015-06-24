3197c637bd9e9c5254b9a09901f033cf2223aea7
fungku <fungku@gmail.com>
2015-06-24 11:30:46 -0700

    initial commit

---
+++ src/Validator.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Fungku\W3FeedValidator;
+
+class Validator
+{
+    public function __construct()
+    {}
+
+    public function validate($feed)
+    {
+        // todo: validate
+    }
+}

5c21fe3e93260212843f8a2e42be1b823201b458
fungku <fungku@gmail.com>
2015-06-25 14:59:42 -0700

    use domdocument instead of simplexmlobject

--- src/W3FeedValidator.php
+++ src/W3FeedValidator.php
@@ -17,51 +17,15 @@ class W3FeedValidator implements FeedValidator
         $validator = "http://validator.w3.org/feed/check.cgi?output=soap12&url={$feed}";
         $response = file_get_contents($validator);
 
-        $xml = $this->makeXML($response);
+        $xml = new \DOMDocument();
+        $xml->loadXML($response);
 
-        return $this->isValid($xml);
-    }
+        $validity = $xml->getElementsByTagName('validity');
 
-    /**
-     * Make a simple xml object from an xml string.
-     *
-     * @param string $xml
-     * @return \SimpleXMLElement
-     */
-    private function makeXML($xml)
-    {
-        $xml = simplexml_load_string($xml, null, null, 'http://schemas.xmlsoap.org/soap/envelope/');
-        $xml->registerXPathNamespace('env', 'http://www.w3.org/2003/05/soap-envelope/');
-        $xml->registerXPathNamespace('m', 'http://www.w3.org/2005/10/markup-validator');
+        if ($validity->length && $validity->item(0)->nodeValue == 'true') {
+            return true;
+        }
 
-        return $xml;
-    }
-
-    /**
-     * Determine if the feed is valid.
-     *
-     * @param \SimpleXMLElement $xml
-     * @return bool
-     */
-    private function isValid(\SimpleXMLElement $xml)
-    {
-        $validity = $xml->xpath('//m:validity');
-        $validity = !empty($validity) ? (string) $validity[0] : '';
-
-        return ('true' === $validity) ? true : false;
-    }
-
-    /**
-     * Get the number of errors.
-     *
-     * @param \SimpleXMLElement $xml
-     * @return int
-     */
-    private function getErrorCount(\SimpleXMLElement $xml)
-    {
-        $errors = $xml->xpath('//m:errorcount');
-        $errors = !empty($errors) ? (int) $errors[0] : 0;
-
-        return $errors < 1;
+        return false;
     }
 }
